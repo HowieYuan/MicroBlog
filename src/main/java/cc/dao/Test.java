5d88ee433b1b53e900f2bfc6105b7899f1988d6f
Your Name <you@example.com>
2017-07-24 19:39:31 +0800

    Initial commit

--- src/main/java/cc/dao/Test.java
+++ src/main/java/cc/dao/Test.java
@@ -10,6 +10,5 @@ public class Test {
          System.out.println(System.getProperty("user.dir"));
          System.out.println(directory.getCanonicalPath());//获取标准的路径 
          System.out.println(directory.getAbsolutePath());//获取绝对路径
-        System.out.println(directory.getAbsolutePath());//获取绝对路径
 	}
 }
